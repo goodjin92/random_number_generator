b7685aa870fe69bc77efbda2c725b55488dff549
Gukjin Kim <exodus0315@outlook.com>
2023-01-29 23:02:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# random_number_generator
+
+Random Number Generator
+
+## 사용된 개념
+
+- Navigation
+- Button
+- Slider
+- Random Number
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,159 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:random_number_generator/component/number_row.dart';
+import 'package:random_number_generator/constant/color.dart';
+import 'package:random_number_generator/screen/settings_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int maxNumber = 1000;
+  List<int> randomNumbers = [
+    123,
+    456,
+    789,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Header(
+                onPressed: onSettingsPop,
+              ),
+              _Body(
+                randomNumbers: randomNumbers,
+              ),
+              _Footer(
+                onPressed: onRandomNumberGenerate,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onSettingsPop() async {
+    final result = await Navigator.of(context).push<int>(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return SettingsScreen(
+            maxNumber: maxNumber,
+          );
+        },
+      ),
+    );
+
+    if (result != null) {
+      setState(() {
+        maxNumber = result;
+      });
+    }
+  }
+
+  void onRandomNumberGenerate() {
+    final rand = Random();
+
+    final Set<int> newNumbers = {};
+
+    while (newNumbers.length != 3) {
+      final number = rand.nextInt(maxNumber);
+      newNumbers.add(number);
+    }
+
+    setState(() {
+      randomNumbers = newNumbers.toList();
+      print(newNumbers);
+    });
+  }
+}
+
+class _Header extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _Header({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤 숫자 생성기',
+          style: TextStyle(
+              color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.w700),
+        ),
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            Icons.settings,
+            color: RED_COLOR,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+
+  const _Body({required this.randomNumbers, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: randomNumbers
+            .asMap()
+            .entries // key와 value로 한번에 가져올 수 있음. key = 몇번째 인덱스를 가졌는지, value는 값을 가져올 때
+            .map(
+              (x) => Padding(
+                padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                child: NumberRow(number: x.value),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  _Footer({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // Container를 써도 되지만, SizedBox를 쓰면 미세하게라도 더 간결하고 빠르고, 이름만 보더라도 목적성이 확실해서 구분이 쉬움
+      width: double.infinity, // 무한으로 설정. 화면 밖으로 나가지는 않음
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: RED_COLOR,
+        ),
+        onPressed: onPressed,
+        child: Text(
+          '생성하기',
+        ),
+      ),
+    );
+  }
+}
